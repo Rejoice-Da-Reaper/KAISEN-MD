de62c5276c900fab3aff8044c59f0dc6610dbba7
Rejoice-Da-Reaper <ighosoturejoice22@gmail.com>
2025-06-10 18:12:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "917003816486",
+DEV: process.env.DEV || "2349118213101",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
